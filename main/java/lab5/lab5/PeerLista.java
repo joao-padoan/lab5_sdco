16afdef71b717504340661bd809cb5bdf989375f
João Pedro Padoan <54078732+joao-padoan@users.noreply.github.com>
2024-06-11 22:30:45 -0300

    Add files via upload

---
+++ main/java/lab5/lab5/PeerLista.java
@@ -0,0 +1,40 @@
+package lab5.lab5;
+
+/**
+ * Lab05: Sistema P2P
+ * 
+ * Autor: Lucio A. Rocha
+ * Ultima atualizacao: 22/05/2023
+ * 
+ * Referencias: 
+ * https://docs.oracle.com/javase/tutorial/essential/io
+ * http://fortunes.cat-v.org/
+ */
+
+public enum PeerLista {
+    
+    PEER1 {
+        @Override
+        public String getNome() {
+            return "PEER1";
+        }        
+    },
+    PEER2 {
+        public String getNome() {
+            return "PEER2";
+        }        
+    },
+    PEER3 {
+        public String getNome() {
+            return "PEER3";
+        }        
+    },
+    PEER4 {
+        public String getNome() {
+            return "PEER4";
+        }        
+    };
+    public String getNome(){
+        return "NULO";
+    }    
+}
